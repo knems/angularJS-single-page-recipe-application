1562ba1a15016afa38de9126eca5ac2dc23f201f
Daniel Boisselle <dboisselle@hotmail.com>
2017-10-20 17:46:57 -0400

    finished project, completely functional recipe app

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -3,10 +3,16 @@
 
 	angular.module('app')
 		.controller('RecipesController', function (dataService, $scope, $location, $route) {
-			$scope.getAllCategories = dataService.getAllCategories(function (response) {
+
+			dataService.getAllRecipes(function(response){
+				$scope.recipes = response.data;
+			});
+
+			dataService.getAllCategories(function (response) {
 				$scope.categories = response.data;
 			});
 
+			//func fires when category is changed and template displays the returned categories
 			$scope.changeCategory = function(category){
 				dataService.getRecipes(category, function(response){
 					if(response.data.length == 0){
@@ -17,16 +23,83 @@
 				});
 			}
 
-			$scope.showRecipe = function(id){
-				$location.path(`edit/:${id}`);
+			$scope.redirect = function(path) {
+				$location.url(path);
+			}
+
+			$scope.delete = function(category, id){
+				dataService.deleteRecipe(id);
+				dataService.getRecipes(category, function(response){
+					if(response.data.length == 0){
+						$scope.recipes = null;
+					}else {
+						$scope.recipes = response.data;
+					}
+				});
 			}
 		});
 
 	angular.module('app')
 		.controller('RecipeDetailController', function(dataService, $scope, $location, $route){
+
+			//finding the id by the change in path, will be an empty array if the path is to add a new recipe
 			let _id = $location.path().split('/').slice(2,3);
-			dataService.getRecipe(_id, function(response){
-				$scope.recipe = response.data;
+
+			//if the path is not empty then
+			if(_id.length !== 0){
+				//update the template with the given recipe id
+				dataService.getRecipe(_id, function(response){
+					$scope.recipe = response.data;
+				});
+				//else we are going to construct a default recipe, where we can set the data
+			}else{
+				$scope.recipe = {
+	        name: "",
+	        description: "",
+	        category: "",
+	        prepTime: "",
+	        cookTime: "",
+	        ingredients: [],
+	        steps: []
+    		};
+			}
+			//setting the categories
+			dataService.getAllCategories(function (response) {
+				$scope.categories = response.data;
 			});
+			//setting the food items
+			dataService.getFoodItems(function (response) {
+				$scope.foodItems = response.data;
+			});
+
+			//func fires when 'Save Recipe' is clicked
+			$scope.redirect = function(path){
+				//if the current recipe is null we will post as a new recipe
+				if(!$scope.recipe._id){
+					dataService.postNewRecipe($scope.recipe);
+				//else means we are modifing an existing recipe and will make the put request
+				}else{
+					dataService.updateRecipe($scope.recipe);
+				}
+				//return to the root url
+				$location.url(path);
+			}
+
+			$scope.cancel = function(){
+				$location.url('/');
+			}
+
+			//general delete func for ingredients and steps
+			$scope.delete = function(index, path){
+				$scope.recipe[path].splice(index, 1);
+			}
+
+			$scope.add = function(args, path){
+				let result = {};
+				args.forEach((value) => {
+					result.value = "";
+				});
+				$scope.recipe[path].push(result);
+			}
 		})
 })();
--- public/scripts/services/data.js
+++ public/scripts/services/data.js
@@ -3,16 +3,41 @@
 
 angular.module('app')
 	.service('dataService', function ($http){
+		//get request to return all recipies
+		this.getAllRecipes = function(callback) {
+			$http.get('http://localhost:5000/api/recipes')
+			.then(callback);
+		}
+		//get request to return all categories
 		this.getAllCategories = function(callback) {
 			$http.get('http://localhost:5000/api/categories')
 			.then(callback);
-		};
+		}
+		//get request to return specific category
 		this.getRecipes = function(category, callback) {
 			$http.get(`http://localhost:5000/api/recipes?category=${category}`)
 			.then(callback);
 		}
+		//get request to return specific recipie by id
 		this.getRecipe = function(recipe_id, callback) {
 			$http.get(`http://localhost:5000/api/recipes/${recipe_id}`)
 			.then(callback);
+		}
+		//get request to return all fooditems avaliable for crafting recipes
+		this.getFoodItems = function(callback) {
+			$http.get(`http://localhost:5000/api/foodItems`)
+			.then(callback);
+		}
+		//post request for a new recipe
+		this.postNewRecipe = function(data) {
+			$http.post(`/api/recipes`, data)
+		}
+		//put request for updating a recipe
+		this.updateRecipe = function(recipe) {
+			$http.put(`/api/recipes/${recipe._id}`, recipe)
+		}
+		//delete request by recipe id
+		this.deleteRecipe = function(id) {
+			$http.delete(`http://localhost:5000/api/recipes/${id}`)
 		}
 	});
